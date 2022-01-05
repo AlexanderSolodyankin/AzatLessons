9d6a57e0b05a918568dd96a9216a7e32eb03d2e7
Alexander <89515014507@mail.ru>
2022-01-05 22:52:38 +0600

    Azat Ferst Work

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Привет Азат
+
+        /**
+         * Тут мы будем писать код
+         */
+    }
+}
